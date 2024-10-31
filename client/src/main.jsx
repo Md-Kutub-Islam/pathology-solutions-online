cf8f9f35bcd35c458a6430e62860dcf57f95ad4d
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-10-31 23:44:00 +0530

    update main.jsx for routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")).render(
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
